cf91e8ad48021f7f1adae84d96bc50be7850f597
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2020-02-14 15:29:56 -0800

    Reorganized imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.DriveAutoTime;
 import frc.robot.commands.DriveTeleOp;
 import frc.robot.subsystems.Drive;
